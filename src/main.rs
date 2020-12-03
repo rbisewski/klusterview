e973f2b0eebb231bded4866fca93523e0c0f61ab
Robert Bisewski <contact@ibiscybernetics.com>
2020-12-03 00:21:14 -0600

    Add string handling

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,47 @@ use std::process::Command;
 //
 async fn print_clusters(_: Request<Body>) -> Result<Response<Body>, Infallible> {
 
-    let output = Command::new("sh")
-                         .arg("-c")
-                         .arg("kubectl config current-context && echo && kubectl get pods --all-namespaces | grep -v kube-system")
-                         .output()
-                         .expect("failed to execute process");
+    let clusters_cmd_output = Command::new("sh")
+                                       .arg("-c")
+                                       .arg("kubectl config get-clusters --kubeconfig=clusters.yaml | sort | grep -v NAME")
+                                       .output()
+                                       .expect("failed to execute process");
 
-    Ok(Response::new(Body::from(output.stdout)))
+    let clusters = match String::from_utf8(clusters_cmd_output.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    let get_pods_cmd_output = Command::new("sh")
+                                       .arg("-c")
+                                       .arg("kubectl config current-context && echo && kubectl get pods --all-namespaces | grep -v kube-system")
+                                       .output()
+                                       .expect("failed to execute process");
+
+    let pods = match String::from_utf8(get_pods_cmd_output.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    let result = [
+        "<html>".to_string(),
+        "<head>".to_string(),
+        "<title>Infrastructure cluster status page</title>".to_string(),
+        "</head>".to_string(),
+        "<body>".to_string(),
+        "<h2>Clusters</h2>".to_string(),
+        "<pre>".to_string(),
+        clusters,
+        "</pre>".to_string(),
+        "<br>".to_string(),
+        "<pre>".to_string(),
+        pods,
+        "</pre>".to_string(),
+        "</body>".to_string(),
+        "</html>".to_string()
+    ].concat();
+
+    Ok(Response::new(Body::from(result)))
 }
 
 //
@@ -31,13 +65,14 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // For every connection, we must make a `Service` to handle all
     // incoming HTTP requests on said connection.
     let make_svc = make_service_fn(|_conn| {
+
         // This is the `Service` that will handle the connection.
         // `service_fn` is a helper to convert a function that
         // returns a Response into a `Service`.
         async { Ok::<_, Infallible>(service_fn(print_clusters)) }
     });
 
-    let addr = ([127, 0, 0, 1], 8080).into();
+    let addr = ([0, 0, 0, 0], 8080).into();
     let server = Server::bind(&addr).serve(make_svc);
 
     println!("Listening on http://{}", addr);
